b040a3cdc606ecb71d3c01a45e4634af386fae65
Roger Franco <rogerfrancogalt@gmail.com>
2023-09-11 09:58:46 -0400

    Pesquisa implementado

--- projetos/agenda/pesquisar.js
+++ projetos/agenda/pesquisar.js
@@ -1,5 +1,6 @@
 const btn_pesq = document.querySelector('#btn_pesq')
 const f_txtpesq = document.querySelector('#f_txtpesq')
+const dados = document.querySelector('#dados')
 
 btn_pesq.addEventListener('click', (evt) => {
   const valor_pesquisado = f_txtpesq.value
@@ -24,5 +25,40 @@ btn_pesq.addEventListener('click', (evt) => {
   let endpoint = `http://localhost:3000/agendas?${query}`
   fetch(endpoint)
   .then(res => res.json())
-  .then(res => console.log(res))
+  .then(res => {
+    dados.innerHTML = "";
+    console.log(res)
+    res.forEach(el => {
+      // console.log('linha')
+      const linha = document.createElement('div')
+      linha.setAttribute('class', 'linhaDados')
+
+      const c1 = document.createElement('div')
+      c1.setAttribute('class', 'coluna c1')
+      c1.innerHTML = el.id
+      linha.appendChild(c1)
+
+      const c2 = document.createElement('div')
+      c2.setAttribute('class', 'coluna c2')
+      c2.innerHTML = el.nome
+      linha.appendChild(c2)
+
+      const c3 = document.createElement('div')
+      c3.setAttribute('class', 'coluna c3')
+      c3.innerHTML = el.celular
+      linha.appendChild(c3)
+
+      const c4 = document.createElement('div')
+      c4.setAttribute('class', 'coluna c4')
+      c4.innerHTML = el.email
+      linha.appendChild(c4)
+
+      const c5 = document.createElement('div')
+      c5.setAttribute('class', 'coluna c5')
+      c5.innerHTML = el.date
+      linha.appendChild(c5)
+
+      dados.appendChild(linha)
+    });
+  })
 })
