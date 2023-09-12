be3d751264aa3e58cd53c2c672ba9069b2e87c6b
Roger Franco <rogerfrancogalt@gmail.com>
2023-09-12 12:58:16 -0400

    Aba de filtrar

---
+++ projetos/agenda/filtrar.js
@@ -0,0 +1,145 @@
+const dados = document.querySelector('#dados')
+const fundopopup = document.querySelector('#fundopopup')
+const btn_gravar = document.querySelector('#btn_gravar')
+const btn_cancelar = document.querySelector('#btn_cancelar')
+const f_id = document.querySelector('#f_id')
+const f_nome = document.querySelector('#f_nome')
+const f_celular = document.querySelector('#f_celular')
+const f_email = document.querySelector('#f_email')
+const f_dtnasc = document.querySelector('#f_dtnasc')
+const btn_filtrar = document.querySelector('#btn_filtrar')
+const f_filtroNome = document.querySelector('#f_filtroNome')
+
+btn_gravar.addEventListener('click', (evt) => {
+  fundopopup.classList.add('ocultar')
+  // console.log(f_id.value, 'ID')
+  // console.log(f_nome.value, 'Nome')
+  // f_id.value = dados[0].innerHTML
+  // f_nome.value = dados[1].innerHTML
+  // f_celular.value = dados[2].innerHTML
+  // f_email.value = dados[3].innerHTML
+  // f_dtnasc.value = dados[4].innerHTML
+  // const id = dados[0].innerHTML
+  const id = f_id.value
+  console.log(id, 'idgravar')
+  const dadosEditar = {
+    'nome': f_nome.value,
+    'celular': f_celular.value,
+    'email': f_email.value,
+    'date': f_dtnasc.value,
+  }
+  console.log(dadosEditar, 'dadosEditar2')
+  const endpoint = `http://localhost:3000/agendas/${id}`
+  const options = {
+    method: "PUT",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(dadosEditar)
+}
+  const response = fetch(endpoint, options)
+  // alert(response)
+console.log(response, 'response')
+alert('ok')
+return response
+
+})
+
+btn_cancelar.addEventListener('click', (evt) => {
+  fundopopup.classList.add('ocultar')
+})
+
+const preencherDataGridView = (endpoint) => {
+  dados.innerHTML = "";
+  // let endpoint = `http://localhost:3000/agendas`
+  fetch(endpoint)
+  .then(res => res.json())
+  .then(res => {
+    console.log(res)
+    res.forEach(el => {
+      // console.log('linha')
+      const linha = document.createElement('div')
+      linha.setAttribute('class', 'linhaDados')
+
+      const c1 = document.createElement('div')
+      c1.setAttribute('class', 'coluna c1')
+      c1.innerHTML = el.id
+      linha.appendChild(c1)
+
+      const c2 = document.createElement('div')
+      c2.setAttribute('class', 'coluna c2')
+      c2.innerHTML = el.nome
+      linha.appendChild(c2)
+
+      const c3 = document.createElement('div')
+      c3.setAttribute('class', 'coluna c3')
+      c3.innerHTML = el.celular
+      linha.appendChild(c3)
+
+      const c4 = document.createElement('div')
+      c4.setAttribute('class', 'coluna c4')
+      c4.innerHTML = el.email
+      linha.appendChild(c4)
+
+      const c5 = document.createElement('div')
+      c5.setAttribute('class', 'coluna c5')
+      c5.innerHTML = el.date
+      linha.appendChild(c5)
+
+      const c6 = document.createElement('div')
+      c6.setAttribute('class', 'coluna c6 c_comando')
+      const img_delete = document.createElement('img')
+      img_delete.setAttribute('src', './imgs/deletar.svg')
+      img_delete.setAttribute('class', 'icone_comando')
+      img_delete.addEventListener('click', (evt) => {
+        // console.log(evt.target.parentNode.parentNode.firstChild.innerHTML)
+        // console.log(evt.target.parentNode.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML)
+        const id = evt.target.parentNode.parentNode.firstChild.innerHTML;
+        removerContato(id)
+      })
+      const img_editar = document.createElement('img')
+      img_editar.setAttribute('src', './imgs/editar.svg')
+      img_editar.setAttribute('class', 'icone_comando')
+      img_editar.addEventListener('click', (evt) => {
+        fundopopup.classList.remove('ocultar')
+        // console.log(evt.target.parentNode.parentNode.childNodes)
+        const dados = evt.target.parentNode.parentNode.childNodes;
+        // const dados = [...evt.target.parentNode.parentNode.childNodes];
+        console.log(dados)
+        f_id.value = dados[0].innerHTML
+        f_nome.value = dados[1].innerHTML
+        f_celular.value = dados[2].innerHTML
+        f_email.value = dados[3].innerHTML
+        f_dtnasc.value = dados[4].innerHTML
+      })
+      c6.appendChild(img_delete)
+      c6.appendChild(img_editar)
+      linha.appendChild(c6)
+
+
+      dados.appendChild(linha)
+    });
+  })
+}
+
+preencherDataGridView('http://localhost:3000/agendas')
+
+const removerContato = (id) => {
+  const endpoint = `http://localhost:3000/agendas/${id}`
+  fetch(endpoint, {
+    method: "DELETE",
+    ContentType: 'application/json'
+  })
+}
+
+
+btn_filtrar.addEventListener('click', (evt) => {
+  console.log(f_filtroNome.value, 'f_filtroNome.value')
+  if(f_filtroNome.value == ''){
+    preencherDataGridView('http://localhost:3000/agendas')
+  }else{
+    const value_input_pesq = f_filtroNome.value
+    preencherDataGridView(`http://localhost:3000/agendas?nome_like=${value_input_pesq}`)
+
+  }
+})
