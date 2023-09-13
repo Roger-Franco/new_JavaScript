a13edddee4ee930dcfa6810d3004c93b17608cd5
Roger Franco <rogerfrancogalt@gmail.com>
2023-09-13 10:20:42 -0400

    Estiliza o menu principal

--- projetos/estoque/backoffice/index.js
+++ projetos/estoque/backoffice/index.js
@@ -1,44 +1,8 @@
-const cabecalho = document.querySelector('#cabecalho')
-const menu = document.querySelector('#menu')
-const btn_home = document.querySelector('#btn_home')
-const btn_novo = document.querySelector('#btn_novo')
-const btn_pesquisar = document.querySelector('#btn_pesquisar')
-const btn_filtrar = document.querySelector('#btn_filtrar')
-const btn_gestao = document.querySelector('#btn_gestao')
-const btn_sobre = document.querySelector('#btn_sobre')
-const principal = document.querySelector('#principal')
+const btn_menuPrincipal = document.querySelector('#btn_menuPrincipal')
+const menuPrincipal = document.querySelector('#menuPrincipal')
+const todosMenusPrincipais = document.querySelector('#btn_menuItem')
 
-btn_home?.addEventListener('click', (evt) => {
-  // console.log(evt.target)
-  abrirPagina(evt.target, './home.html')
+
+btn_menuPrincipal.addEventListener("click", (evt) => {
+  menuPrincipal.classList.toggle('ocultar')
 })
-
-btn_novo?.addEventListener('click', (evt) => {
-  abrirPagina(evt.target, './novo.html')
-})
-
-btn_pesquisar?.addEventListener('click', (evt) => {
-  abrirPagina(evt.target, './pesquisar.html')
-})
-
-btn_filtrar?.addEventListener('click', (evt) => {
-  abrirPagina(evt.target, './filtrar.html')
-})
-
-btn_gestao?.addEventListener('click', (evt) => {
-  abrirPagina(evt.target, './gestao.html')
-})
-
-btn_sobre?.addEventListener('click', (evt) => {
-  abrirPagina(evt.target, './sobre.html')
-})
-
-const abrirPagina = (el, url) => {
-  const abas = [...document.querySelectorAll('.aba')]; // NodeList para Array
-  // console.log(abas)
-  abas.forEach((e) => {
-    e.classList.remove('abaSelecionada')
-  })
-  el.classList.add('abaSelecionada')
-  window.open(url, 'if_principal')
-}
