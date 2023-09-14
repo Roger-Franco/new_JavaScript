25b695e86b1cf6d258664f72014aa4d9d98be13f
Roger Franco <rogerfrancogalt@gmail.com>
2023-09-14 00:08:03 -0400

    Menus e inicio da tela Dos Colaboradores

---
+++ projetos/estoque/backoffice/colaboradores/colaboradores.js
@@ -0,0 +1,9 @@
+// const endpoint_todosColaboradores = 'algum_endpoint'
+
+// fetch(endpoint_todosColaboradores)
+// .then(res => res,json())
+// .then(res => {
+
+// })
+
+console.log('Chegou aqui noColaboradores.js')
--- projetos/estoque/backoffice/index.js
+++ projetos/estoque/backoffice/index.js
@@ -1,8 +1,14 @@
 const btn_menuPrincipal = document.querySelector('#btn_menuPrincipal')
 const menuPrincipal = document.querySelector('#menuPrincipal')
-const todosMenusPrincipais = document.querySelector('#btn_menuItem')
+const todosMenusPrincipais = [...document.querySelectorAll('.btn_menuItem')];
 
 
 btn_menuPrincipal.addEventListener("click", (evt) => {
   menuPrincipal.classList.toggle('ocultar')
+})
+
+todosMenusPrincipais.forEach(e => {
+  e.addEventListener('click', (evt) => {
+    menuPrincipal.classList.add('ocultar')
+  })
 })
